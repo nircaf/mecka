fb77929eaf2d9642b39fd9593fedc44c6103bf8d
Nircaf <nircaf@gmail.com>
2024-01-10 10:36:41 +0200

    a

--- sele.py
+++ sele.py
@@ -22,6 +22,46 @@ except:
         cred = {'email': email, 'password': password}
         yaml.dump(cred, f)
 
+
+def send_email_empty(txt):
+    # read cred.yaml
+    with open('cred.yaml', 'r', encoding='utf-8') as f:
+        cred = yaml.load(f, Loader=yaml.FullLoader)
+    import email, smtplib, ssl
+
+    from email import encoders
+    from email.mime.base import MIMEBase
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.text import MIMEText
+
+    subject = f'{txt}'
+    body = f"""{txt} \n
+    \n Thanks, \n Nir"""
+    sender_email = cred['email_to_send']
+    recipients = ["nir@shopperai.ai"]
+    receiver_email = ", ".join(recipients)
+    password = cred['password_to_send']
+
+    for reci in recipients:
+        # Create a multipart message and set headers
+        message = MIMEMultipart()
+        message["From"] = sender_email
+        message["To"] = reci
+        message["Subject"] = subject
+        # message["Bcc"] = ''  # Recommended for mass emails
+
+        # Add body to email
+        message.attach(MIMEText(body, "plain"))
+
+        # Add attachment to message and convert message to string
+        text = message.as_string()
+
+        # Log in to server using secure context and send email
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, reci, text)
+
 # Set up Chrome options
 options = Options()
 # options.add_argument("--headless")  # Ensure GUI is off
@@ -53,15 +93,29 @@ time.sleep(2)
 login_button = driver.find_element(By.NAME, 'submit')
 login_button.click()
 try:
+    bool_checkout = True
     # Find the div by its class name
     clock_div = wait.until(EC.element_to_be_clickable((By.ID, 'checkout-button')))
     # Click the div
     clock_div.click()
 except:
+    bool_checkout = False
     # <a id="checkin-button" href="#" onclick="return false;">
     clock_div = wait.until(EC.element_to_be_clickable((By.ID, 'checkin-button')))
     # Click the div
     clock_div.click()
 # sleep 5
 time.sleep(5)
-pass
+# verify click
+if bool_checkout:
+    try:
+        clock_div = wait.until(EC.element_to_be_clickable((By.ID, 'checkin-button')))
+    except:
+        str_email = "Failed to checkout"
+else:
+    try:
+        clock_div = wait.until(EC.element_to_be_clickable((By.ID, 'checkout-button')))
+    except:
+        str_email = "Failed to checkin"
+
+send_email_empty(str_email)
